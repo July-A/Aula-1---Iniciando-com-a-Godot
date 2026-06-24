55e4cc1a35ab2836d262779116922ba05727a88b
July-A <vjulya40@gmail.com>
2026-06-24 11:29:23 -0300

    Ac-2 Hud+ menu

---
+++ games-ju/Script/menu_inicial.gd
@@ -0,0 +1,3 @@
+extends Control
+func _on_iniciar_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://level.tscn")
---
+++ games-ju/game_manager.gd
@@ -0,0 +1,15 @@
+# Arquivo game_manager.gd
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+# variável para controlar as vidas
+	var vidas: int = 3
